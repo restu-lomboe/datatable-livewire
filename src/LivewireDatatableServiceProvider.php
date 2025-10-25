32aea232cef2be7329bd4fb3ba94c6bb42821685
Restu <restulomboe0117@gmail.com>
2025-10-25 16:40:47 +0700

    fix: fix bug auto load package pdf and excel

--- src/LivewireDatatableServiceProvider.php
+++ src/LivewireDatatableServiceProvider.php
@@ -3,6 +3,8 @@
 namespace Developerawam\LivewireDatatable;
 
 use Illuminate\Support\ServiceProvider;
+use Maatwebsite\Excel\ExcelServiceProvider;
+use Barryvdh\DomPDF\ServiceProvider as PDFServiceProvider;
 
 class LivewireDatatableServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,10 @@ class LivewireDatatableServiceProvider extends ServiceProvider
             ], 'livewire-datatable-config');
         }
 
+        // Register Excel and PDF facades
+        $this->app->alias('Excel', \Maatwebsite\Excel\Facades\Excel::class);
+        $this->app->alias('PDF', \Barryvdh\DomPDF\Facade\Pdf::class);
+
         \Livewire\Livewire::component('livewire-datatable', \Developerawam\LivewireDatatable\Components\DataTable::class);
     }
 
@@ -33,5 +39,11 @@ class LivewireDatatableServiceProvider extends ServiceProvider
     {
         // Automatically apply the package configuration
         $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'livewire-datatable');
+
+        // Register Excel Service Provider
+        $this->app->register(ExcelServiceProvider::class);
+
+        // Register DomPDF Service Provider
+        $this->app->register(PDFServiceProvider::class);
     }
 }
